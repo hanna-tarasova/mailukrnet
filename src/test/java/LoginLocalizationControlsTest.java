47a94380fbc83eec17e17d848f3ea1328f526ef4
hanna-tarasova <hanna_a@ukr.net>
2019-05-03 12:36:37 +0300

    new version

--- src/test/java/LoginLocalizationControlsTest.java
+++ src/test/java/LoginLocalizationControlsTest.java
@@ -20,38 +20,47 @@ import static com.codeborne.selenide.Selenide.open;
             }
 
             @Test
-            public void checkControlsLocalization() throws InterruptedException {
+            public void checkControlsLocalizationUkr() throws InterruptedException {
                 $(By.xpath("//div[2]/button[1]/span[1]")).shouldBe(Condition.text("Українська")).click();
                 //включаємо українську мову
 
 
-
                 $(By.cssSelector(".button")).isDisplayed();
-
-                //$("#ytLogout").shouldBe(Condition.visible).waitUntil(Condition.visible, 30000).click();
-
-
 
                 SoftAssert softAssertion = new SoftAssert();
                 softAssertion.assertEquals("Увійти", $(By.cssSelector(".button")).getText(), "Неправильний текст для кнопки логіну");
-              //softAssertion.assertEquals("Не вдається уувійти", $(By.cssSelector(".form__controls")).getText(), "Неправильний текст для кнопки відновлення доступу");
+                //softAssertion.assertEquals("Не вдається уувійти", $(By.cssSelector(".form__controls")).getText(), "Неправильний текст для кнопки відновлення доступу");
                 softAssertion.assertEquals("Не вдається увійти?", $(By.xpath("//div[4]/a[1]")).getText(), "Неправильний текст для кнопки відновлення доступу (укр)");
                 softAssertion.assertEquals("Створити скриньку", $(By.xpath("//div[4]/a[2]")).getText(), "Неправильний текст для кнопки реєстрації (укр)");
-                Thread.sleep(2000);
+                Thread.sleep(9000);
+                softAssertion.assertAll();}
 
 
+
+            @Test
+            public void checkControlsLocalizationRus() throws InterruptedException {
 
                 $(By.xpath("//div[2]/button[2]/span[1]")).shouldBe(Condition.text("Русский")).click();
+                //включаємо рос мову
+                $(By.cssSelector(".button")).isDisplayed();
+                SoftAssert softAssertion = new SoftAssert();
                 softAssertion.assertEquals("Войти", $(By.cssSelector(".button")).getText(), "Неправильний текст для кнопки логіну (рус)");
                 softAssertion.assertEquals("Не удается войти?", $(By.xpath("//div[4]/a[1]")).getText(), "Неправильний текст для кнопки відновлення доступу (рус)");
                 softAssertion.assertEquals("Создать ящик", $(By.xpath("//div[4]/a[2]")).getText(), "Неправильний текст для кнопки реєстрації (рус)");
-                Thread.sleep(2000);
+                Thread.sleep(9000);
+                softAssertion.assertAll();
+            };
 
+            @Test
+            public void checkControlsLocalizationEng() throws InterruptedException {
 
                 $(By.xpath("//div[2]/button[3]/span[1]")).shouldBe(Condition.text("English")).click();
+                //включаємо англ мову
+                $(By.cssSelector(".button")).isDisplayed();
+                SoftAssert softAssertion = new SoftAssert();
                 softAssertion.assertEquals("Trouble signing in?", $(By.xpath("//div[4]/a[1]")).getText(), "Неправильний текст для кнопки відновлення доступу (англ)");
                 softAssertion.assertEquals("Sign up", $(By.xpath("//div[4]/a[2]")).getText(), "Неправильний текст для кнопки реєстрації (англ)");
-                Thread.sleep(2000);
+                Thread.sleep(9000);
                 softAssertion.assertAll();}
 
             }
